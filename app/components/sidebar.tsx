95eb87cb10d66ed232769b293b7f7aaaedc7d7c9
Kinda Hall <1142704468@qq.com>
2023-09-14 20:55:13 +0800

    feat: 🎸 修改文档

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -118,11 +118,9 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          ChatGPT Next
+          AI助手
         </div>
-        <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
-        </div>
+        <div className={styles["sidebar-sub-title"]}>为你答题解惑。</div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
         </div>
